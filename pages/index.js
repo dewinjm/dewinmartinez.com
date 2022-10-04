3af8285fe010a17037a2a61224244713066edcad
dewinjm <dewin.martinez@gmail.com>
2022-10-03 20:58:26 -0400

    chore: change footer

--- pages/index.js
+++ pages/index.js
@@ -24,21 +24,14 @@ export default function Home() {
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>          
+          </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>
+          Dewin J. Martínez © 2022. All Right Reserved.
+        </p>
       </footer>
     </div>
   )
