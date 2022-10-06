1e1812ab394c5e542223dedd52fd4394dcbaffb9
dewinjm <dewin.martinez@gmail.com>
2022-10-06 16:20:09 -0400

    fix: sentry config files

--- next.config.js
+++ next.config.js
@@ -36,7 +36,6 @@ const sentryWebpackPluginOptions = {
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
-  dryRun: process.env.NODE_ENV !== 'production',
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
